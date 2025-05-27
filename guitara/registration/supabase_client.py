62e6dfaeeceff9f11f16e3d22670e5e92c7564ba
Jon Gleur Tan <qjgptan@tip.edu.ph>
2025-05-27 17:19:55 +0800

    Update supabase client and add migration to remove driver and therapist fields

--- guitara/registration/supabase_client.py
+++ guitara/registration/supabase_client.py
@@ -1,5 +1,10 @@
+import os
 from supabase import create_client, Client
+from dotenv import load_dotenv
 
-url = "https://cpxwkxtbjzgmjgxpheiw.supabase.co"
-key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNweHdreHRianpnbWpneHBoZWl3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDMyNDMwNjQsImV4cCI6MjA1ODgxOTA2NH0._tLksGoARKXHE4b-bqSlf_Eoygs3ATQClXVr5iGnsOw"
+load_dotenv()  # Loads .env automatically
+
+url = os.getenv("VITE_SUPABASE_URL")
+key = os.getenv("VITE_SUPABASE_ANON_KEY")
+
 supabase: Client = create_client(url, key)
